c5edb8c01903477869839949566491545100fdba
mateusmax076 <72174578+mateusmax076@users.noreply.github.com>
2020-12-04 20:46:46 -0300

    atualização mateus

--- application/controllers/Saidapresos.php
+++ application/controllers/Saidapresos.php
@@ -4,7 +4,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Saidapresos extends CI_Controller{
 
     public function index(){
+        $this->db->select('*');
+        $dados['tbl_saidadetentos'] = $this->db->get('tbl_saidadetentos')->result();
+        $this->load->view('Saida-presos-view',$dados);
 
-        $this->load->view('saida-presos-view');
+
+
     }
+
+
+    
+	public function usuarios(){
+		return $this->db->get('tbl_saidadetentos')->result_array();
+	}
+     
+    public function pesquisar(){
+        
+        $this->load->model('cadastrar_sair_model','tbl_saidadetentos');
+        $dados['tbl_saidadetentos'] = $this->cadastrar_sair->get_cadastrar_sair_like();
+        $this->load->view('Saida-presos-view',$dados);
+     
+        
+    
+
+    }
+     
+
+
 }
--- application/controllers/SaidapresosAudiencia.php
+++ application/controllers/SaidapresosAudiencia.php
@@ -2,9 +2,34 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SaidapresosAudiencia extends CI_Controller{
+    public function __construct(){
+        parent:: __construct();
+        $this->load->model('SaidapresosAudiencia_model');//carregando o model dos presos
+    }
 
     public function index(){
-
-        $this->load->view('saida-presosAudiencia-view');
+        
+        $this->db->select('*');
+        $dados['tbl_saidaaudiencia'] = $this->db->get('tbl_saidaaudiencia')->result();
+        $this->load->view('saida-presosAudiencia-view',$dados);
     }
+
+    public function audiencia(){ // Chama a função responsável pelo cadastro dos presos ao banco de dados
+        $this->load->view('cadastrar_audiencia_view');
+    }
+
+    public function pesquisar(){
+        
+        $this->load->model('SaidapresosAudiencia_model','tbl_saidaaudiencia');
+        $dados['tbl_saidaaudiencia'] = $this->cadastrar_sair->get_cadastrar_sair_like();
+        $this->load->view('saida-presosAudiencia-view',$dados);
+        
+    }
+
+    public function create(){ // Chama a função responsável pelo cadastro dos presos ao banco de dados
+        $this->SaidapresosAudiencia_model->SaidapresosAudiencia();
+        redirect('saidapresosAudiencia');
+    }
+
+
 }
